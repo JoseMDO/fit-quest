3db71398b4a3f7dd8ec4b2b16cb40fb45979297c
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-11-20 19:46:02 -0600

    added main pages and routes for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ExerciseDetail from './pages/ExerciseDetail'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise" element={<ExerciseDetail />} />
+            <Route path="/login" element={<Login />} /> 
+        </Routes>
+    </BrowserRouter>
   )
 }
 
