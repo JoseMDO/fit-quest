ba4a53c454a1e6ae316e28e2360cb00e088ac75f
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-11-22 01:43:16 -0600

    login working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const Home = () => {
+
+  const logout = () => {
+    window.open("http://localhost:3000/auth/logout", "_self");
+}
   return (
-    <div className='text-4xl'>Home</div>
+    <div className='text-4xl'>
+      <p>Home</p>
+      <div onClick={logout}>Logout</div>
+    </div>
+    
   )
 }
 
